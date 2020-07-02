ba646bbfb461cd0e46122b66ac390c288291b786
William Jackson <william@subtlecoolness.com>
2020-07-02 11:39:23 -0500

    Shellcheck

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ PENDULUM_VERSION="$(/usr/bin/awk -F '==' '/pendulum/ { print $2 }' requirements.
 /usr/bin/git clone --branch "${PENDULUM_VERSION}" --depth 1 https://github.com/sdispater/pendulum.git
 /usr/bin/curl --location https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py --output /root/get-poetry.py
 /usr/local/bin/python /root/get-poetry.py
-cd pendulum
+cd pendulum || exit
 /root/.poetry/bin/poetry build

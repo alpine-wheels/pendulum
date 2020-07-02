2c3ec36ab3f560f36478a28fe8567a85f95de844
William Jackson <william@subtlecoolness.com>
2020-07-02 10:15:18 -0500

    Update build script to read from requirements.txt

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-/usr/bin/git clone --branch 2.1.0 --depth 1 https://github.com/sdispater/pendulum.git
+PENDULUM_VERSION="$(/usr/bin/awk -F '==' '/pendulum/ { print $2 }' requirements.txt)"
+/usr/bin/git clone --branch "${PENDULUM_VERSION}" --depth 1 https://github.com/sdispater/pendulum.git
 /usr/bin/curl --location https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py --output /root/get-poetry.py
 /usr/local/bin/python /root/get-poetry.py
 cd pendulum

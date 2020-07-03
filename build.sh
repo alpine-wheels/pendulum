c3b27c939829347fe49f64268814ae565aefe548
William Jackson <william@subtlecoolness.com>
2020-07-03 11:16:45 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -3,20 +3,8 @@
 set -e
 
 # install os dependencies
-apk add --no-cache curl git
-
-# find version
-PENDULUM_VERSION="$(awk -F '==' '/pendulum/ { print $2 }' requirements.txt)"
-
-# check out source code
-rm -rf pendulum
-git config --global advice.detachedHead false
-git clone --branch "${PENDULUM_VERSION}" --depth 1 https://github.com/sdispater/pendulum.git
-
-# install poetry
-curl --location https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py --output /root/get-poetry.py
-python /root/get-poetry.py
+apk add --no-cache gcc libffi-dev musl-dev openssl-dev
 
 # build
-cd pendulum || exit
-/root/.poetry/bin/poetry build
+pip install poetry
+pip wheel --no-build-isolation --no-deps --requirement requirements.txt

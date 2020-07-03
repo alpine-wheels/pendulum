41b08b04f3c8dfaee6aa064696071742d3b8838a
William Jackson <william@subtlecoolness.com>
2020-07-02 20:46:54 -0500

    Do not use custom builder image

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
-PENDULUM_VERSION="$(/usr/bin/awk -F '==' '/pendulum/ { print $2 }' requirements.txt)"
-/usr/bin/git clone --branch "${PENDULUM_VERSION}" --depth 1 https://github.com/sdispater/pendulum.git
-/usr/bin/curl --location https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py --output /root/get-poetry.py
-/usr/local/bin/python /root/get-poetry.py
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache curl git
+
+# find version
+PENDULUM_VERSION="$(awk -F '==' '/pendulum/ { print $2 }' requirements.txt)"
+
+# check out source code
+rm -rf pendulum
+git config --global advice.detachedHead false
+git clone --branch "${PENDULUM_VERSION}" --depth 1 https://github.com/sdispater/pendulum.git
+
+# install poetry
+curl --location https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py --output /root/get-poetry.py
+python /root/get-poetry.py
+
+# build
 cd pendulum || exit
 /root/.poetry/bin/poetry build

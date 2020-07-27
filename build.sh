7ee36a90824e0ed81b71a4b3fc59833fd89aeb6f
William Jackson <william@subtlecoolness.com>
2020-07-27 08:53:29 -0500

    pendulum==2.1.2

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-# install os dependencies
-apk add --no-cache gcc libffi-dev musl-dev openssl-dev
+# install dependencies
+#apk add --no-cache gcc libffi-dev musl-dev openssl-dev
 
 # build
-pip install --extra-index-url https://alpine-wheels.github.io/index/ poetry
-pip wheel --no-build-isolation --no-deps --requirement requirements.txt
+pip wheel --no-deps --requirement requirements.txt
